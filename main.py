fa71349a7a4727110cbc16a153f4c40a034cd95c
Dan-96 <daniel.michalczyk96@gmail.com>
2023-04-01 20:59:02 +0100

    Image fixes and code optimizations.
    Changed code structure to class based and changed player's hair colour

--- main.py
+++ main.py
@@ -1,13 +1,97 @@
 import pygame
 from sys import exit
-from random import randint
+from random import randint, choice
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
+        player_run_1 = pygame.image.load('Graphics/Player_run_1.png').convert_alpha()
+        player_run_2 = pygame.image.load('Graphics/Player_run_2.png').convert_alpha()
+        player_run_3 = pygame.image.load('Graphics/Player_run_3.png').convert_alpha()
+        player_run_4 = pygame.image.load('Graphics/Player_run_4.png').convert_alpha()
+        player_run_5 = pygame.image.load('Graphics/Player_run_5.png').convert_alpha()
+        player_run_6 = pygame.image.load('Graphics/Player_run_6.png').convert_alpha()
+        player_run_7 = pygame.image.load('Graphics/Player_run_7.png').convert_alpha()
+        player_run_8 = pygame.image.load('Graphics/Player_run_8.png').convert_alpha()
+        player_run_9 = pygame.image.load('Graphics/Player_run_9.png').convert_alpha()
+        self.player_run = [
+            player_run_1,
+            player_run_2,
+            player_run_3,
+            player_run_4,
+            player_run_5,
+            player_run_6,
+            player_run_7,
+            player_run_8,
+            player_run_9,
+        ]
+        self.player_index = 0
+        self.player_jump = pygame.image.load('Graphics/Player_jump.png').convert_alpha()
+
         self.image = pygame.image.load('Graphics/Player_run_1.png').convert_alpha()
-        self.rect = self.image.get_rect(midbottom = (200, 300))
+        self.rect = self.image.get_rect(midbottom = (80, 300))
+        self.gravity = 0
+
+    def player_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] and self.rect.bottom >= 300:
+            self.gravity = -15
+
+    def apply_gravity(self):
+        self.gravity += 1
+        self.rect.y += self.gravity
+        if self.rect.bottom >= 300:
+            self.rect.bottom = 300
+
+    def animation_state(self):
+        if self.rect.bottom < 300:
+            self.image = self.player_jump
+        else:
+            self.player_index += 0.3
+            if self.player_index >= len(self.player_run):
+                self.player_index = 0
+            self.image = self.player_run[int(self.player_index)]
+
+    def update(self):
+        self.player_input()
+        self.apply_gravity()
+        self.animation_state()
+
+
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self, type):
+        super().__init__()
+        if type == 'bird':
+            bird_frame_1 = pygame.image.load('Graphics/Enemy_bird_1.png').convert_alpha()
+            bird_frame_2 = pygame.image.load('Graphics/Enemy_bird_2.png').convert_alpha()
+            self.frames = [bird_frame_1, bird_frame_2]
+            y_pos = 160
+        else:
+            boulder_frame_1 = pygame.image.load('Graphics/Enemy_boulder_1.png').convert_alpha()
+            boulder_frame_2 = pygame.image.load('Graphics/Enemy_boulder_2.png').convert_alpha()
+            self.frames = [boulder_frame_1, boulder_frame_2]
+            y_pos = 300
+
+        self.animation_index = 0
+
+        self.image = self.frames[self.animation_index]
+        self.rect = self.image.get_rect(midbottom = (randint(900, 1100), y_pos))
+
+    def animation_state(self):
+        self.animation_index += 0.1
+        if self.animation_index >= len(self.frames):
+            self.animation_index = 0
+        self.image = self.frames[int(self.animation_index)]
+
+    def destroy(self):
+        if self.rect.x < -200:
+            self.kill()
+
+    def update(self):
+        self.animation_state()
+        self.rect.x -= 15
+        self.destroy()
 
 
 def display_score():
@@ -17,41 +101,12 @@ def display_score():
     screen.blit(score_surf, score_rect)
     return current_time
 
-
-def obstacle_movement(obstacle_list):
-    if obstacle_list:
-        for obstacle_rect in obstacle_list:
-            obstacle_rect.left -= 15
-            if obstacle_rect.bottom == 300:
-                screen.blit(boulder_surf, obstacle_rect)
-            else:
-                screen.blit(bird_surf, obstacle_rect)
-        obstacle_list = [obstacle for obstacle in obstacle_list if obstacle.left > -200]
-        return obstacle_list
+def sprite_collisions():
+    if pygame.sprite.spritecollide(player_group.sprite, obstacle_group, False):
+        obstacle_group.empty()
+        return False
     else:
-        return []
-
-
-def collisions(player, obstacles):
-    if obstacles:
-        for obstacle_rect in obstacles:
-            if player.colliderect(obstacle_rect):
-                return False
-    return True
-
-
-def player_animations():
-    global player_surf, player_index
-    if player_rect.bottom < 300:
-        # Jumping animation
-        player_surf = player_jump
-    else:
-        # Walking animation
-        player_index += 0.3
-        if player_index >= len(player_run):
-            player_index = 0
-        player_surf = player_run[int(player_index)]
-
+        return True
 
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
@@ -61,8 +116,11 @@ clock = pygame.time.Clock()
 font = pygame.font.Font('Fonts/MP16REG.ttf', 50)
 game_active = True
 start_time = 0
-player = pygame.sprite.GroupSingle()
-player.add(Player())
+
+player_group = pygame.sprite.GroupSingle()
+player_group.add(Player())
+
+obstacle_group = pygame.sprite.Group()
 
 # Background and ground
 background_surf = pygame.transform.scale((pygame.image.load('Graphics/Background.png').convert()), (800, 400))
@@ -83,41 +141,6 @@ bird_frames = [bird_frame_1, bird_frame_2]
 bird_frame_index = 0
 bird_surf = bird_frames[int(bird_frame_index)]
 
-# Obstacle list
-obstacle_rect_list = []
-
-# Player walk
-player_run_1 = pygame.image.load('Graphics/Player_run_1.png').convert_alpha()
-player_run_2 = pygame.image.load('Graphics/Player_run_2.png').convert_alpha()
-player_run_3 = pygame.image.load('Graphics/Player_run_3.png').convert_alpha()
-player_run_4 = pygame.image.load('Graphics/Player_run_4.png').convert_alpha()
-player_run_5 = pygame.image.load('Graphics/Player_run_5.png').convert_alpha()
-player_run_6 = pygame.image.load('Graphics/Player_run_6.png').convert_alpha()
-player_run_7 = pygame.image.load('Graphics/Player_run_7.png').convert_alpha()
-player_run_8 = pygame.image.load('Graphics/Player_run_8.png').convert_alpha()
-player_run_9 = pygame.image.load('Graphics/Player_run_9.png').convert_alpha()
-
-player_run = [
-    player_run_1,
-    player_run_2,
-    player_run_3,
-    player_run_4,
-    player_run_5,
-    player_run_6,
-    player_run_7,
-    player_run_8,
-    player_run_9,
-]
-
-# Player jump
-player_jump = pygame.image.load('Graphics/Player_jump.png').convert_alpha()
-
-player_index = 0
-
-player_surf = player_run[player_index]
-player_rect = player_surf.get_rect(midbottom = (50, 300))
-player_gravity = 0
-
 # Timer
 obstacle_timer = pygame.USEREVENT + 1
 pygame.time.set_timer(obstacle_timer, 700)
@@ -135,10 +158,7 @@ while True:
             exit()
         if game_active:
             if event.type == obstacle_timer:
-                if randint(0, 2):
-                    obstacle_rect_list.append(boulder_frame_1.get_rect(midbottom = (randint(900, 1100), 300)))
-                else:
-                    obstacle_rect_list.append(bird_frame_1.get_rect(midbottom=(randint(900, 1100), 150)))
+                obstacle_group.add(Obstacle(choice(['bird', 'boulder', 'boulder', 'boulder'])))
 
             if event.type == boulder_animation_timer:
                 if boulder_frame_index == 0:
@@ -154,16 +174,6 @@ while True:
                     bird_frame_index = 0
                 bird_surf = bird_frames[bird_frame_index]
 
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    if player_rect.bottom >= 300:
-                        player_gravity = -16
-
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if player_rect.collidepoint(event.pos):
-                    if player_rect.bottom >= 300:
-                        player_gravity = -16
-
         else:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
@@ -176,23 +186,14 @@ while True:
         score = display_score()
 
         # Player
-        player_gravity += 1
-        if player_rect.bottom >= 300:
-            player_rect.bottom = 300
-        player_animations()
-        player.draw(screen)
-        screen.blit(player_surf, player_rect)
-        player_rect.top += player_gravity
+        player_group.update()
+        player_group.draw(screen)
+        obstacle_group.draw(screen)
+        obstacle_group.update()
+        game_active = sprite_collisions()
 
-        # Obstacle movement
-        obstacle_rect_list = obstacle_movement(obstacle_rect_list)
-
-        # Collisions with enemy
-        game_active = collisions(player_rect, obstacle_rect_list)
 
     else:
-        obstacle_rect_list.clear()
-        player_rect.midbottom = (50, 300)
         player_gravity = 0
         score_surf2 = font.render('PRESS SPACE BAR TO RESTART', False, 'white')
         score_rect2 = score_surf2.get_rect(center=(400, 200))
